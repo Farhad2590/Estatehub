ac2827f02161ff16f9e5aab60381eacd93fe4ed6
Farhad2590 <farhadhossen2590@gmail.com>
2024-04-15 15:01:01 +0600

    2 new section which is GlamourEstateHub Packages and GlamourEstateHub Services added and Footer Part Desgined:

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,11 @@
 // import { NavLink } from "react-router-dom";
+import logo from "../assets/Logo.png"
 
 const Navbar = () => {
     const Navs =
         <>
             <li><a>Home</a></li>
-            <li><a>Saved Vilas</a></li>
-            <li><a>User Profile</a></li>
+            <li><a>Update Profile</a></li>
         </>
     return (
         <div className="navbar bg-base-100">
@@ -15,10 +15,15 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                       {Navs}
+                        {Navs}
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">GlamourEstateHub</a>
+                <a className="flex">
+                    <div className="flex items-center justify-center w-12 h-12 rounded-full dark:bg-violet-600">
+                        <img src={logo} alt="" />
+                    </div>
+                    <span className="self-center text-3xl font-semibold">GlamourEstateHub</span>
+                </a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
--- src/Components/SpecialChoose.jsx
+++ src/Components/SpecialChoose.jsx
@@ -1,8 +1,17 @@
 import { TbHomeCog } from "react-icons/tb";
+import { GrShieldSecurity } from "react-icons/gr";
+import { GrTechnology } from "react-icons/gr";
+import { GrVmMaintenance } from "react-icons/gr";
+import { FaHouseFloodWaterCircleArrowRight } from "react-icons/fa6";
+import { FaPersonShelter } from "react-icons/fa6";
+
+
+
+
 
 const SpecialChoose = () => {
     return (
-        <div className="bg-orange-50 mt-10 mb-10">
+        <div className="bg-orange-50 my-10">
             <div data-aos="fade-right" data-aos-duration="1000" className="py-5">
                 <h1 className="text-3xl font-bold pt-5 px-5">GlamourEstateHub Services</h1>
                 <h6 className="text-xl font-medium pt-3 pb-5 px-5">Why Choose Us? lets see our Services</h6>
@@ -14,48 +23,52 @@ const SpecialChoose = () => {
                     />
                     <div className="text-center">
                         <h4 className="text-xl font-bold">Property Management</h4>
-                        <p>
-                            A property manager handles all daily operations of a large estate, from staff oversight to maintenance and repairs.</p>
+                        <p>A property manager handles all daily operations of a large estate, from staff oversight to maintenance and repairs.</p>
                     </div>
                 </div>
                 <div className="flex flex-col items-center bg-white p-5">
-                    <TbHomeCog className="text-7xl text-yellow-600" />
+                    <GrShieldSecurity
+                        className="text-7xl text-yellow-600 transition-transform duration-500 ease-in-out hover:rotate-360"
+                    />
                     <div className="text-center">
-                        <h4 className="text-xl font-bold">Property Management</h4>
-                        <p>
-                            A property manager handles all daily operations of a large estate, from staff oversight to maintenance and repairs.</p>
+                        <h4 className="text-xl font-bold">Security Services</h4>
+                        <p>Luxury estates require comprehensive security,gates and surveillance,cybersecurity,private security personnel.</p>
                     </div>
                 </div>
                 <div className="flex flex-col items-center bg-white p-5">
-                    <TbHomeCog className="text-7xl text-yellow-600" />
+                    <GrTechnology
+                        className="text-7xl text-yellow-600 transition-transform duration-500 ease-in-out hover:rotate-360"
+                    />
                     <div className="text-center">
-                        <h4 className="text-xl font-bold">Property Management</h4>
-                        <p>
-                            A property manager handles all daily operations of a large estate, from staff oversight to maintenance and repairs.</p>
+                        <h4 className="text-xl font-bold">Technology Integration</h4>
+                        <p>Luxury estate owners rely on specialized technicians to seamlessly integrate cutting-edge smart home systems, from security to entertainment, ensuring optimal comfort and convenience.</p>
                     </div>
                 </div>
                 <div className="flex flex-col items-center bg-white p-5">
-                    <TbHomeCog className="text-7xl text-yellow-600" />
+                    <GrVmMaintenance
+                        className="text-7xl text-yellow-600 transition-transform duration-500 ease-in-out hover:rotate-360"
+                    />
                     <div className="text-center">
-                        <h4 className="text-xl font-bold">Property Management</h4>
-                        <p>
-                            A property manager handles all daily operations of a large estate, from staff oversight to maintenance and repairs.</p>
+                        <h4 className="text-xl font-bold">Design and Maintenance</h4>
+                        <p>Professional landscaping services for luxury estates ensure the maintenance and design of expansive grounds, catering to various functions from parties to private retreats.</p>
                     </div>
                 </div>
                 <div className="flex flex-col items-center bg-white p-5">
-                    <TbHomeCog className="text-7xl text-yellow-600" />
+                    <FaHouseFloodWaterCircleArrowRight
+                        className="text-7xl text-yellow-600 transition-transform duration-500 ease-in-out hover:rotate-360"
+                    />
                     <div className="text-center">
-                        <h4 className="text-xl font-bold">Property Management</h4>
-                        <p>
-                            A property manager handles all daily operations of a large estate, from staff oversight to maintenance and repairs.</p>
+                        <h4 className="text-xl font-bold">Housekeeping and Staffing Services</h4>
+                        <p>Estate owners rely on specialized staffing services to source, hire, and manage dedicated domestic staff adept at maintaining high-end properties.</p>
                     </div>
                 </div>
                 <div className="flex flex-col items-center bg-white p-5">
-                    <TbHomeCog className="text-7xl text-yellow-600" />
+                    <FaPersonShelter
+                        className="text-7xl text-yellow-600 transition-transform duration-500 ease-in-out hover:rotate-360"
+                    />
                     <div className="text-center">
-                        <h4 className="text-xl font-bold">Property Management</h4>
-                        <p>
-                            A property manager handles all daily operations of a large estate, from staff oversight to maintenance and repairs.</p>
+                        <h4 className="text-xl font-bold">Personal Concierge</h4>
+                        <p>A personal concierge provides comprehensive assistance, from travel arrangements to event planning, ensuring estate owners can enjoy their time stress-free.</p>
                     </div>
                 </div>
             </div>
@@ -64,3 +77,11 @@ const SpecialChoose = () => {
 };
 
 export default SpecialChoose;
+
+// Personal Concierge: A personal concierge can handle various tasks such as making travel arrangements, booking appointments, organizing events, and securing hard-to-get reservations. This service allows estate owners to enjoy their time without worrying about the logistics of daily activities and special occasions.
+
+// Home Automation and Technology Integration: To keep up with the latest in home technology, luxury estate owners often need technicians who specialize in integrating smart home systems. This can include everything from advanced security systems to smart lighting, heating, and cooling systems, audio-visual and entertainment systems, and even home assistants.
+
+// Landscape Design and Maintenance: Given the extensive grounds that often accompany luxury estates, professional landscaping services are necessary. These services not only maintain the beauty of the gardens and grounds but can also design outdoor spaces for specific functions such as parties, relaxation, or private retreats.
+
+// Housekeeping and Staffing Services: Maintaining a large estate requires a dedicated team of housekeepers, chefs, butlers, and other staff. Estate owners often employ staffing services that specialize in sourcing, hiring, and managing domestic staff who understand the nuances of working in high-end properties.
